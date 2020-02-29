4f4244f33be132a878b0f81e47e9655d5d4d45d4
kosaraju <gopala.kosaraju@ing.nl>
2020-02-29 19:24:10 +0100

    fixed errors during manual testing for CategoryByUUID

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/PaymentDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/PaymentDao.java
@@ -1,12 +1,11 @@
 package com.upgrad.FoodOrderingApp.service.dao;
 
 import com.upgrad.FoodOrderingApp.service.entity.PaymentEntity;
-import org.springframework.stereotype.Repository;
-
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
-import java.util.List;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public class PaymentDao {
@@ -26,7 +25,8 @@ public class PaymentDao {
     //Return payment method by paymet UUID
     public PaymentEntity getMethodbyId(final String uuid){
         try{
-            return entityManager.createNamedQuery("getMethodbyId", PaymentEntity.class).setParameter("uuid", uuid).getSingleResult();
+          return entityManager.createNamedQuery("getMethodbyId", PaymentEntity.class)
+              .setParameter("paymentUUID", uuid).getSingleResult();
         }catch(NoResultException nre){
             return null;
         }
